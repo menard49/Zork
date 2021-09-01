bafe544229c67045b0b5ebabcd9fb14b1141e7a6
menard49 <menard8282@gmail.com>
2021-09-01 14:56:41 -0400

    Zork 1.2
    Finding Your Direction

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace Zork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Zork!");
+
+            string inputString = Console.ReadLine();
+            Commands command = ToCommand(inputString.Trim().ToUpper());
+            Console.WriteLine(command);
+        }
+
+        private static Commands ToCommand(string commandString)
+        {
+            return Enum.TryParse<Commands>(commandString, out Commands command) ? command : Commands.UNKNOWN; 
+        }
+    }
+}
